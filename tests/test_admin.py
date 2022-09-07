38c562e47667acc08fc8ebc9afaa211d400f2db5
Adam Murray <adam-murray@hotmail.co.uk>
2022-09-07 11:31:49 +0100

    fix: Improved test coverage

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,24 +1,23 @@
-from django.contrib import admin
 from django.shortcuts import reverse
 
 from cms.models import Site
 from cms.test_utils.testcases import CMSTestCase
 
-from aldryn_redirects.models import StaticRedirect
+from aldryn_redirects.models import Redirect, StaticRedirect
 
 from bs4 import BeautifulSoup
 
 
-class StaticRedirectRootTestCase(CMSTestCase):
+class AdminRedirectRootTestCase(CMSTestCase):
     def setUp(self):
         self.user = super().get_superuser()
-        self.site = admin.AdminSite()
+        self.site = Site.objects.first()
         self.add_url = reverse("admin:aldryn_redirects_staticredirect_add")
 
     def test_static_redirect_allows_root(self):
         add_url = reverse("admin:aldryn_redirects_staticredirect_add")
         redirect_data = {
-            "sites": [Site.objects.first().id, ],
+            "sites": [self.site.id, ],
             "inbound_route": "/",
             "outbound_route": "/test-page",
             'query_params-TOTAL_FORMS': '1',
@@ -38,3 +37,22 @@ class StaticRedirectRootTestCase(CMSTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(expected_result, result.text)
         self.assertEqual(StaticRedirect.objects.all().count(), 1)
+
+    def test_multilingual_redirect_allows_root(self):
+        add_url = reverse("admin:aldryn_redirects_redirect_add")
+        redirect_data = {
+            "site": [self.site.id],
+            "old_path": "/",
+        }
+
+        with self.login_user_context(self.user):
+            response = self.client.post(add_url, data=redirect_data, follow=True)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        result = soup.find("li", {"class": "success"})
+        expected_inbound = redirect_data["old_path"]
+        expected_result = f"The Multilanguage Redirect “{expected_inbound} ---> en:” was added successfully"
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(expected_result, result.text)
+        self.assertEqual(Redirect.objects.all().count(), 1)
